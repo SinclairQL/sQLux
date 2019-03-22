SinclairQL/uqlx-fork

unixstuff/uqlx_cfg.c : move AUTO_BOOT to config file
Request from Martin Head/Tim Swensont to move the AUTO_BOOT
config for skipping the F1/F2 on startup to the config file
so it can be user configured!

New option is

SKIP_BOOT = 0/1 (where 1 is default if not defined)

Signed-off-by: Graeme Gregory <graeme@xora.org.uk>

unixstuff.c
@@ -71,9 +71,7 @@ extern void FlushDisplay (void);
 
 extern void DbgInfo (void);
 
-#ifdef AUTO_BOOT
 void btrap3 (void);
-#endif
 
 extern void SchedulerCmd (void);
 extern void KbdCmd (void);
@@ -1098,9 +1096,9 @@ void uqlxInit ()
       table[KBENC_CMD_CODE] = KBencCmd;
    }
    table[BASEXT_CMD_CODE]=BASEXTCmd;
-#ifdef AUTO_BOOT
-   table[0x4e43] = btrap3;
-#endif
+
+   if (QMD.skip_boot)
+   	table[0x4e43] = btrap3;
   
 #ifdef NEW_AREG
    g_reg = reg;

SinclairQL/sQLux

unixstuff.c
@@ -72,9 +72,7 @@ extern void FlushDisplay (void);
 
 extern void DbgInfo (void);
 
-#ifdef AUTO_BOOT
 void btrap3 (void);
-#endif
 
 extern void SchedulerCmd (void);
 extern void KbdCmd (void);
@@ -1073,9 +1071,9 @@ void uqlxInit ()
       qlux_table[KBENC_CMD_CODE] = KBencCmd;
    }
    qlux_table[BASEXT_CMD_CODE]=BASEXTCmd;
-#ifdef AUTO_BOOT
-   qlux_table[0x4e43] = btrap3;
-#endif
+
+   if (QMD.skip_boot)
+	qlux_table[0x4e43] = btrap3;
   
    g_reg = reg;
 