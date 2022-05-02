7a39d48bdf2b704c4f73108ae853aafe90d419a5
Graeme Gregory <graeme@xora.org.uk>
2022-05-02 20:02:01 +0100

    build-all.sh : make release builds
    Signed-off-by: Graeme Gregory <graeme@xora.org.uk>

--- build-all.sh
+++ build-all.sh
@@ -6,22 +6,22 @@ if [ "$ARCH" != "x86_64" ]; then
     echo "Script assumes x86_64 host for building, exiting..."
 fi
 
-cmake -B x86_64/
+cmake -B x86_64/ -DCMAKE_BUILD_TYPE=Release
 cmake --build x86_64/
 
-cmake -B x86_32/ -DCMAKE_TOOLCHAIN_FILE=x86_32.cmake
+cmake -B x86_32/ -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=x86_32.cmake
 cmake --build x86_32/
 
-cmake -B w64 -DCMAKE_TOOLCHAIN_FILE=mingw-w64-x86_64.cmake -DCMAKE_PREFIX_PATH=/usr/local/x86_64-w64-mingw32
+cmake -B w64/ -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=mingw-w64-x86_64.cmake -DCMAKE_PREFIX_PATH=/usr/local/x86_64-w64-mingw32
 cmake --build w64
 
-cmake -B w32 -DCMAKE_TOOLCHAIN_FILE=mingw-w64-i686.cmake -DCMAKE_PREFIX_PATH=/usr/local/i686-w64-mingw32
+cmake -B w32/ -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=mingw-w64-i686.cmake -DCMAKE_PREFIX_PATH=/usr/local/i686-w64-mingw32
 cmake --build w32
 
 if [ "x$PI_HOST" != "x" ]; then
 
 	rsync --progress --delete --recursive --exclude=x86_64 --exclude=x86_32 --exclude=w64 --exclude=w32 --exclude=armv6 --exclude=armv7 --exclude=arm64 . $PI_HOST:tmp-build/
-	ssh $PI_HOST "cd tmp-build;cmake -B armv6/;cmake --build armv6/"
+	ssh $PI_HOST "cd tmp-build;cmake -B armv6/ -DCMAKE_BUILD_TYPE=Release ; cmake --build armv6/"
 	rsync --progress --delete --recursive $PI_HOST:tmp-build/armv6 .
 	ssh $PI_HOST "rm -rv tmp-build"
 fi
@@ -29,7 +29,7 @@ fi
 if [ "x$ARM64_HOST" != "x" ]; then
 
 	rsync --progress --delete --recursive --exclude=x86_64 --exclude=x86_32 --exclude=w64 --exclude=w32 --exclude=armv6 --exclude=armv7 --exclude=arm64 . $ARM64_HOST:tmp-build/
-	ssh $ARM64_HOST "cd tmp-build;cmake -B arm64/;cmake --build arm64/"
+	ssh $ARM64_HOST "cd tmp-build;cmake -B arm64/ -DCMAKE_BUILD_TYPE=Release ; cmake --build arm64/"
 	rsync --progress --delete --recursive $ARM64_HOST:tmp-build/arm64 .
 	ssh $ARM64_HOST "rm -rv tmp-build"
 fi
@@ -37,7 +37,7 @@ fi
 if [ "x$ARMV7_HOST" != "x" ]; then
 
 	rsync --progress --delete --recursive --exclude=x86_64 --exclude=x86_32 --exclude=w64 --exclude=w32 --exclude=armv6 --exclude=armv7 --exclude=arm64 . $ARMV7_HOST:tmp-build/
-	ssh $ARMV7_HOST "cd tmp-build;cmake -B armv7/;cmake --build armv7/"
+	ssh $ARMV7_HOST "cd tmp-build;cmake -B armv7/ -DCMAKE_BUILD_TYPE=Release ; cmake --build armv7/"
 	rsync --progress --delete --recursive $ARMV7_HOST:tmp-build/armv7 .
 	ssh $ARMV7_HOST "rm -rv tmp-build"
 fi
