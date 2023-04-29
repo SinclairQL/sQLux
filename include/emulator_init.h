956963d6b697124d02eedb7618eff8e7b6267358
Graeme Gregory <graeme@xora.org.uk>
2023-04-29 19:33:29 +0100

    include/emulator_init.h : add missing include
    Signed-off-by: Graeme Gregory <graeme@xora.org.uk>

---
+++ include/emulator_init.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifndef EMULATOR_INIT_H
+#define EMULATOR_INIT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int emulatorLoadRom(char *romDir, char *romName, uint32_t addr, size_t size);
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif /* EMULATOR_INIT_H */
